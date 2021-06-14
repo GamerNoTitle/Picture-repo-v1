ea7f39bad1ef526ca223fcd38804886b9d4e8874
GamerNoTitle <bili33@87ouo.top>
2021-06-14 16:15:17 +0800

    Added Some Pictures for my Hexo Blog

--- js/301Redirect.js
+++ js/301Redirect.js
@@ -1,8 +1,10 @@
 url = window.location.href
 whether = String(url).search(/20[0-2][0-9]\/[0-1][1-9]\/[0-3][1-9]/i)
+alert(whether)
 if(whether == True){
-result = String(url).replace(/20[0-2][0-9]\/[0-1][1-9]\/[0-3][1-9]/i,'posts')
-window.location = result}
+    result = String(url).replace(/20[0-2][0-9]\/[0-1][1-9]\/[0-3][1-9]/i,'posts')
+    window.location = result
+}
 else{
     window.location = 'https://404.bili33.top'
 }
